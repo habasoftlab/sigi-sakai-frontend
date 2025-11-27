8e9627e40c13dc549cbaf507f80e66c7f5b423bb
IsaiasJA <159166127+IsaiasJA@users.noreply.github.com>
2025-11-26 18:08:23 -0600

    New window for requesting supplies #29

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-
 import Link from 'next/link';
 import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
@@ -7,7 +6,7 @@ import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
-    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
+    const { layoutConfig, setLayoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
@@ -17,6 +16,21 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         topbarmenu: topbarmenuRef.current,
         topbarmenubutton: topbarmenubuttonRef.current
     }));
+
+    const toggleTheme = () => {
+        const nextScheme = layoutConfig.colorScheme === 'light' ? 'dark' : 'light';
+        const nextTheme = nextScheme === 'light' ? 'lara-light-blue' : 'lara-dark-purple';
+        const themeLink = document.getElementById('theme-css') as HTMLLinkElement;
+        const themeHref = `/themes/${nextTheme}/theme.css`;
+        if (themeLink) {
+            themeLink.href = themeHref;
+        }
+        setLayoutConfig((prevState) => ({
+            ...prevState,
+            colorScheme: nextScheme,
+            theme: nextTheme
+        }));
+    };
 
     return (
         <div className="layout-topbar">
@@ -34,18 +48,15 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
             </button>
 
             <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
+                <button type="button" className="p-link layout-topbar-button" onClick={toggleTheme}>
+                    <i className={`pi pi-${layoutConfig.colorScheme === 'light' ? 'moon' : 'sun'}`}></i>
+                    <span>Cambiar Tema</span>
+                </button>
+
                 <button type="button" className="p-link layout-topbar-button">
                     <i className="pi pi-bell"></i>
-                    <span>Notifications</span>
+                    <span>Notificaciones</span>
                 </button>
-                <button type="button" className="p-link layout-topbar-button">
-                    <i className="pi pi-user"></i>
-                    <span>Profile</span>
-                </button>
-                    <button type="button" className="p-link layout-topbar-button">
-                        <i className="pi pi-cog"></i>
-                        <span>Settings</span>
-                    </button>
             </div>
         </div>
     );
